465e48b09fe84f2720b097bc7effa3bd93531659
Zentla <51496839+Zentla@users.noreply.github.com>
2021-05-04 23:13:38 -0500

    animacion

---
+++ pages/app.js
@@ -0,0 +1,22 @@
+//variables
+let animado = document.querySelectorAll('#animado');
+
+
+//eventlistener
+
+
+//funciones
+function mostrarScroll() {
+    let scrollTop = document.documentElement.scrollTop;
+    
+    for ( var i=0; i < animado.length; i++ ){
+        let alturaAnimado = animado[i].offsetTop;
+        if (alturaAnimado < scrollTop) {
+          animado[i].style.opacity = 1;  
+        }
+    }
+};
+
+window.addEventListener('scroll', mostrarScroll);
+
+
